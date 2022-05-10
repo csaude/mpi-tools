f9b1ab29121a8eb9230c2706b6a0f25b2d930474
JPboane <jpboane@gmail.com>
2022-05-10 14:04:09 +0200

    Added more fields to MatchedRecord

--- src/main/java/com/mpi/tools/api/model/MatchedRecord.java
+++ src/main/java/com/mpi/tools/api/model/MatchedRecord.java
@@ -20,56 +20,91 @@ public class MatchedRecord {
 	private Long id;
     
     @NotNull
-    @Column(name = "date_created", nullable = false, updatable = false)
+    @Column(updatable = false)
     private Date dateCreated;
     
 	@Column(nullable = false, unique = false)
 	private String opencr_cruid;
 
-	@Column(nullable = false, unique = false)
+	@NotNull
+	@Column(unique = false)
 	private String cruid;
 	
 	@JoinColumn(name="match_issue_id", nullable=false)
 	@ManyToOne(optional=false)
 	private MatchIssue matchIssue;
 
-	public Date getDateCreated() {
-		return dateCreated;
-	}
-
-	public void setDateCreated(Date dateCreated) {
-		this.dateCreated = dateCreated;
-	}
-
+	private String givenName;
+	private String familyName;
+	private String tarvNid;
+	private String gender;
+	private String birthDate;
+	private Boolean transferedTo;
+	
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
 	public String getOpencr_cruid() {
 		return opencr_cruid;
 	}
-
 	public void setOpencr_cruid(String opencr_cruid) {
 		this.opencr_cruid = opencr_cruid;
 	}
-
 	public String getCruid() {
 		return cruid;
 	}
-
 	public void setCruid(String cruid) {
 		this.cruid = cruid;
 	}
-
 	public MatchIssue getMatchIssue() {
 		return matchIssue;
 	}
-
 	public void setMatchIssue(MatchIssue matchIssue) {
 		this.matchIssue = matchIssue;
+	}
+	public String getGivenName() {
+		return givenName;
+	}
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+	public String getFamilyName() {
+		return familyName;
+	}
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+	public String getTarvNid() {
+		return tarvNid;
+	}
+	public void setTarvNid(String tarvNid) {
+		this.tarvNid = tarvNid;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public Boolean getTransferedTo() {
+		return transferedTo;
+	}
+	public void setTransferedTo(Boolean transferedTo) {
+		this.transferedTo = transferedTo;
 	}
 }
--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -8,11 +8,16 @@ create table mpi_matched_record (id bigint(20) NOT NULL AUTO_INCREMENT,
 								openmrs_uuid varchar(36) NOT NULL,
 								opencr_cruid varchar(36) NOT NULL,
 								match_issue_id bigint(20) NOT NULL, 
+								given_name varchar(250) NULL,
+								family_name varchar(250) NULL,
+								tarv_nid  varchar(250) NULL,
+								gender varchar(15) NOT NULL,
+								birth_date varchar(20),
+								transfered_to int(1),
 								PRIMARY KEY (id),
 								UNIQUE KEY `unq_matched_openmrs_uuid` (`match_issue_id`, `openmrs_uuid`),
 								UNIQUE KEY `unq_matched_opencr_cruid` (`match_issue_id`, `opencr_cruid`),
 								CONSTRAINT `matched_record_fk1` FOREIGN KEY (`match_issue_id`) REFERENCES `mpi_match_issue` (`id`)
  
 							) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-							
 							

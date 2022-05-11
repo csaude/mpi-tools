0dd196d780213d5b7faaf16ca2d62e2b18d23fba
JPboane <jpboane@gmail.com>
2022-05-11 08:47:14 +0200

    Modified db script to includ cruid on match_issue

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,5 +1,7 @@
 create table mpi_match_issue (	id bigint(20) NOT NULL AUTO_INCREMENT, 
 								date_created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+								opencr_cruid varchar(36) NOT NULL,
+								UNIQUE KEY `unq_match_issue_opencr_cruid` (`match_issue_id`, `opencr_cruid`),
 								PRIMARY KEY (id)
 							) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -18,6 +20,5 @@ create table mpi_matched_record (id bigint(20) NOT NULL AUTO_INCREMENT,
 								UNIQUE KEY `unq_matched_openmrs_uuid` (`match_issue_id`, `openmrs_uuid`),
 								UNIQUE KEY `unq_matched_opencr_cruid` (`match_issue_id`, `opencr_cruid`),
 								CONSTRAINT `matched_record_fk1` FOREIGN KEY (`match_issue_id`) REFERENCES `mpi_match_issue` (`id`)
- 
 							) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 							

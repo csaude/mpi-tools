83a9c503c815695dbd6eb12c72316e42b93d19d8
Daniel Chirinda <chirindzadaniel@gmail.com>
2022-05-12 15:27:06 +0200

    Run patient match on startup

--- src/main/java/com/mpi/tools/MpiToolsApiApplication.java
+++ src/main/java/com/mpi/tools/MpiToolsApiApplication.java
@@ -1,15 +1,35 @@
 package com.mpi.tools;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import com.mpi.tools.api.resource.MatchedResource;
 
 @SpringBootApplication
 @EnableFeignClients
 public class MpiToolsApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MpiToolsApiApplication.class, args);
-    }
+	@Autowired
+	private MatchedResource matchedResource;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpiToolsApiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner CommandLineRunnerBean() {
+		return (args) -> {
+			System.out.println("Finding Match Patient");
+
+			matchedResource.findAllMatched();
+
+			System.out.println("Done Finding Match Patient");
+
+		};
+	}
 
 }
